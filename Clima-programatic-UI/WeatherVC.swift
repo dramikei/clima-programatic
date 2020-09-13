c0e12538eaae00cebdb04587810bc64aefea3043
dramikei <raghav2011rv@gmail.com>
2020-09-14 00:32:05 +0530

    Added and configured top bar stackView with location, searchField, searchBtn

--- Clima-programatic-UI/WeatherVC.swift
+++ Clima-programatic-UI/WeatherVC.swift
@@ -14,18 +14,65 @@ class WeatherVC: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+    
+    var topStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    var locationBtn: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        return button
+    }()
+    
+    var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .systemFill
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Search"
+        return textField
+    }()
+    
+    var searchBtn: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        return button
+    }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(backgroundImage)
+        view.addSubview(topStackView)
         configureBgImageView()
-        
+        configureTopStackView()
     }
     
     
-
+    private func configureTopStackView() {
+        topStackView.translatesAutoresizingMaskIntoConstraints = false
+        topStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        topStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        topStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        topStackView.addArrangedSubview(locationBtn)
+        topStackView.addArrangedSubview(searchTextField)
+        topStackView.addArrangedSubview(searchBtn)
+    }
     
     
     private func configureBgImageView() {
